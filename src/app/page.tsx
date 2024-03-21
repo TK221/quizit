0cd025bf1a5773e431c37565cc9c4052c8167c3d
TK221 <51761737+TK221@users.noreply.github.com>
2024-03-21 16:13:41 +0100

    fix: correct home page description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex flex-col items-center">
       <h1 className="mt-20 text-8xl font-bold">Quizit</h1>
       <p className="mt-5 text-xl">
-        A platform to create and share quizzes with your friends
+        A platform for tracking points and buzzing in quizzes
       </p>
     </div>
   );
