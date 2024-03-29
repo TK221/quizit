8b02fe1da382ddca7444ee7d10755ed5c6847690
TK221 <51761737+TK221@users.noreply.github.com>
2024-03-29 23:05:06 +0100

    feat: add buzzing sound

--- src/app/_components/lobby/buzz-info.tsx
+++ src/app/_components/lobby/buzz-info.tsx
@@ -1,14 +1,26 @@
 "use client";
 
 import type Pusher from "pusher-js";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Card, CardHeader, CardTitle } from "../ui/card";
 import type Lobby from "./lobby";
 import HandleAnswer from "./handle-answer";
 import { IsGameMasterContext } from "./lobby";
 
+const audio = new Audio("/sounds/buzzing-sound.mp3");
+
 const BuzzInfo = (props: { pusher: Pusher; lobby: Lobby }) => {
   const isGameMaster = useContext(IsGameMasterContext);
+
+  useEffect(() => {
+    props.pusher.bind("buzz", async () => {
+      await audio.play();
+    });
+
+    return () => {
+      props.pusher.unbind("buzz");
+    };
+  }, [props.pusher]);
 
   return (
     <div>
