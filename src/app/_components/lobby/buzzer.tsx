ee49d82b54dc1a7199ae2bac3f8e282589ae1aff
TK221 <51761737+TK221@users.noreply.github.com>
2024-04-02 18:29:37 +0200

    feat: buzz with space press

--- src/app/_components/lobby/buzzer.tsx
+++ src/app/_components/lobby/buzzer.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { api } from "~/trpc/react";
 import { Button } from "../ui/button";
 
 const Buzzer = (props: { lobbyId: string; lobbyState: boolean }) => {
   const buzz = api.lobby.buzz.useMutation();
 
+  const handleBuzz = () => {
+    buzz.mutate({ lobbyId: props.lobbyId });
+  };
+
+  const handleSpacePress = (event: KeyboardEvent) => {
+    if (event.key === " ") handleBuzz();
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleSpacePress);
+
+    return () => {
+      window.removeEventListener("keydown", handleSpacePress);
+    };
+  });
+
   return (
     <Button
       variant={props.lobbyState ? "destructive" : "outline"}
       className="h-52 w-52"
-      onClick={() => buzz.mutate({ lobbyId: props.lobbyId })}
+      onClick={() => handleBuzz()}
     >
       {props.lobbyState ? "BUZZ" : "CLOSED"}
     </Button>
